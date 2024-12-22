7ac20152cc39c1ea5fc5f9084ac2d1bc17dbc759
YannToukam <yannyvan2005@gmail.com>
2024-12-22 15:21:08 -0500

    Update server.py

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,73 +1,80 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-from stockfish import Stockfish
+from io import StringIO
 import chess
 import chess.pgn
 import tqdm
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from stockfish import Stockfish
 
 app = Flask(__name__)
-CORS(app) 
+CORS(app)
 
-# Initialize Stockfish engine
+# Initialize Stockfish engine and thresholds
 stockfish_path = r"C:\Users\yanny\Downloads\stockfish-windows-x86-64-avx2\stockfish\stockfish-windows-x86-64-avx2.exe"
 stockfish = Stockfish(stockfish_path)
 stockfish.set_depth(16)
 stockfish.set_skill_level(16)
 
-# Inaccuracy between 30 to <50 in stockfish eval change
 INACCURACY_THRESHOLD = 30
-
-# Mistake between 50> to <150 in stockfish eval change
 MISTAKE_THRESHOLD = 50
-
-# Blunder more than 150> cp in stockfish eval change
 BLUNDER_THRESHOLD = 150
 
-#METTRE LA GAME SOUS FICHIER CSV
 
 @app.route('/analyze', methods=['POST'])
 def analyze_pgn():
     try:
-        pgn_data = request.json['pgn']
-        print(f"Received PGN data: {pgn_data}")
+        # Receive and log incoming PGN data
+        pgn_data = request.json.get('pgn')
+        if not pgn_data:
+            return jsonify({'error': 'PGN data not found in request'}), 400
 
-        # Load the PGN
-        game = chess.pgn.read_game(chess.StringIO(pgn_data))
+        print(pgn_data)
+
+        pgn_io = StringIO(pgn_data)
+        game = chess.pgn.read_game(pgn_io)
+        if game is None:
+            return jsonify({'error': 'Invalid PGN data'}), 400
 
         # Process moves
-        board = game.board()
+        board = chess.Board()
         move_cp_value = []
         type_errors = {}
 
-        def error_classifier(cp1, cp2, player_move):
+        def error_classifier(cp1, cp2, player_move, board_fen):
+            error_type = None
             if INACCURACY_THRESHOLD <= (cp1 - cp2) < MISTAKE_THRESHOLD:
-                type_errors[player_move] = 'inaccuracy'
+                error_type = 'Inaccuracy'
             elif MISTAKE_THRESHOLD <= (cp1 - cp2) < BLUNDER_THRESHOLD:
-                type_errors[player_move] = 'mistake'
+                error_type = 'Mistake'
             elif (cp1 - cp2) >= BLUNDER_THRESHOLD:
-                type_errors[player_move] = 'blunder'
+                error_type = 'Blunder'
 
-        for i, move in tqdm.tqdm(enumerate(game.mainline_moves())):
+            if error_type:
+                type_errors[player_move] = {'error_type': error_type, 'board_fen': board_fen}
+
+        for i, move in enumerate(tqdm.tqdm(game.mainline_moves())):
             board.push(move)
 
-            if i % 2 == 0:  # It's white's move
+            if i % 2 == 0:
                 stockfish.set_fen_position(board.fen())
                 move_evaluation = stockfish.get_evaluation()['value']
                 move_cp_value.append(move_evaluation)
                 eval_index = i // 2
                 if 0 < eval_index < len(move_cp_value):
-                    error_classifier(move_cp_value[eval_index - 1], move_cp_value[eval_index], move.uci())
+                    error_classifier(move_cp_value[eval_index - 1], move_cp_value[eval_index], move.uci(), previous_board)
+            previous_board = board.fen()
 
-        print(type_errors)
-        print(move_cp_value)
+        print(f"Error moves from the player: {type_errors}")
+        # print(move_cp_value)
 
-        return jsonify({'message': 'PGN data received and processed successfully'})
-    
+        return jsonify({'message': 'PGN data received and processed successfully', 'errors': type_errors})
+
     except KeyError:
         return jsonify({'error': 'PGN data not found in request'}), 400
-    
+
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
