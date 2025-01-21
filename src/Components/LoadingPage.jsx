649a8030fcc8d36707ae57578a6b0b1eaf247f9b
YannToukam <149633415+YannToukam@users.noreply.github.com>
2025-01-20 23:53:41 -0600

    Add files via upload

---
+++ src/Components/LoadingPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function LoadingPage() {
+  return (
+    <div className="loading-container">
+      <div className="loading-spinner"></div>
+      <h2>Loading game errors...</h2>
+
+    </div>
+  );
+}
+
+export default LoadingPage;
